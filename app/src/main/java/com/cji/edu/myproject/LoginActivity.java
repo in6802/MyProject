152fcd9a2fdbc8f4b3447906e1a93796f34c3d0c
in6802 <in6802@gmail.com>
2018-10-23 13:42:37 +0900

    first 2 commit

--- app/src/main/java/com/cji/edu/myproject/LoginActivity.java
+++ app/src/main/java/com/cji/edu/myproject/LoginActivity.java
@@ -158,7 +158,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         boolean cancel = false;
         View focusView = null;
-
+//
         // Check for a valid password, if the user entered one.
         if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
             mPasswordView.setError(getString(R.string.error_invalid_password));
